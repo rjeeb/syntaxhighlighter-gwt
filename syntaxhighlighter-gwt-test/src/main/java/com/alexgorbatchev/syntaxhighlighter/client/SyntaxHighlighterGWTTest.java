f1074e336253f1a1a9826c66494c80f99d0dd5ba
Matt Davis <matt.davis@temis.us>
2014-04-22 12:57:48 -0400

    simply example

--- syntaxhighlighter-gwt-test/src/main/java/com/alexgorbatchev/syntaxhighlighter/client/SyntaxHighlighterGWTTest.java
+++ syntaxhighlighter-gwt-test/src/main/java/com/alexgorbatchev/syntaxhighlighter/client/SyntaxHighlighterGWTTest.java
@@ -2,22 +2,17 @@ package com.alexgorbatchev.syntaxhighlighter.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class SyntaxHighlighterGWTTest implements EntryPoint {
 	
 	@Override
 	public void onModuleLoad() {
+		
 		Highlighter.loadBrushes(Brush.Brushes.XML());
-		final Highlighter highlighter = new Highlighter(Brush.Brushes.XML(), "<test>\n  <item></item>\n</test>");
 		
-		highlighter.setWidth("200px");
-		highlighter.setHeight("200px");
+		Highlighter highlighter = new Highlighter(Brush.Brushes.XML(), "<test>\n  <item></item>\n</test>");
 		
-		VerticalPanel vp = new VerticalPanel();
-		vp.add(highlighter);
-		
-		RootLayoutPanel.get().add(vp);
+		RootLayoutPanel.get().add(highlighter);
 		
 	}
 }
